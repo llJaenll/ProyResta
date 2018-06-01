c42acdc02d980bc42969b183193e1d02da527398
llJaenll <llJeanll@outlook.cl>
2018-06-01 00:30:30 -0500

    SubidaLogin
    SubidaLogin

--- ProyLPII/src/service/UsuarioService.java
+++ ProyLPII/src/service/UsuarioService.java
@@ -1,11 +1,72 @@
 package service;
 
+import java.util.List;
+
+import beans.TipoUsuarioDTO;
+import beans.UsuarioDTO;
+import beans.UsuarioTipoDTO;
 import dao.DAOFactory;
 import interfaces.DetalleDeliveryDAO;
+import interfaces.UsuarioDAO;
 
-public class UsuarioService {
+public class UsuarioService implements UsuarioDAO{
 	
 	DAOFactory fabrica = DAOFactory.getDAOFactory(DAOFactory.MYSQL);
-	DetalleDeliveryDAO dao =  fabrica.getDetalleDeliveryDAO();	
+	UsuarioDAO dao =  fabrica.getUsuarioDAO();
+	@Override
+	public int registrar(UsuarioDTO c) {
+		// TODO Auto-generated method stub
+		return dao.registrar(c);
+	}
+	@Override
+	public int eliminar(int codigo) {
+		// TODO Auto-generated method stub
+		return dao.eliminar(codigo);
+	}
+	@Override
+	public int modificar(UsuarioDTO c) {
+		// TODO Auto-generated method stub
+		return dao.modificar(c);
+	}
+	@Override
+	public List<UsuarioDTO> listadoUsuario() {
+		// TODO Auto-generated method stub
+		return dao.listadoUsuario();
+	}
+	@Override
+	public UsuarioDTO validarUsuario(String nusuario, String contraseña) {
+		// TODO Auto-generated method stub
+		return dao.validarUsuario(nusuario, contraseña);
+	}
+	@Override
+	public UsuarioDTO buscarUsuario(int codigo) {
+		// TODO Auto-generated method stub
+		return dao.buscarUsuario(codigo);
+	}
+	@Override
+	public List<TipoUsuarioDTO> listarTipoUsaurio() {
+		// TODO Auto-generated method stub
+		return dao.listarTipoUsaurio();
+	}
+	@Override
+	public List<UsuarioTipoDTO> listarUsuarioTipo() {
+		// TODO Auto-generated method stub
+		return dao.listarUsuarioTipo();
+	}
+	@Override
+	public UsuarioDTO buscarUsuarioTipo(int codigo) {
+		// TODO Auto-generated method stub
+		return dao.buscarUsuarioTipo(codigo);
+	}
+	@Override
+	public int cantidadUsuario() {
+		// TODO Auto-generated method stub
+		return dao.cantidadUsuario();
+	}
+	@Override
+	public int validarDni(String dni) {
+		// TODO Auto-generated method stub
+		return dao.validarDni(dni);
+	}	
 
 }
---
+++ ProyLPII/src/servlets/ServletLogin.java
@@ -0,0 +1,59 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import beans.UsuarioDTO;
+import service.UsuarioService;
+
+/**
+ * Servlet implementation class ServletLogin
+ */
+@WebServlet("/ServletLogin")
+public class ServletLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		//variables
+		
+				String usuario,clave;
+				
+				//entradas
+				
+				usuario = request.getParameter("usuario");
+				clave= request.getParameter("password");
+				
+				//procesos
+				
+				UsuarioService service = new UsuarioService();
+				UsuarioDTO u = service.validarUsuario(usuario, clave);
+				
+				//salidas
+				
+				if(u!=null) {
+					
+					//éxito
+					request.setAttribute("msj", "Buena esa");
+					request.getRequestDispatcher("/loginAdministracion.jsp").forward(request, response);
+					
+				}else {
+					request.setAttribute("msj", "Credenciales Invalidas");
+					request.getRequestDispatcher("/loginAdministracion.jsp").forward(request, response);
+					
+					
+					
+				}
+		
+		
+	}
+
+}
